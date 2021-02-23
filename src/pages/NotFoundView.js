64dcacaf61b4ca09fd985a8ec7705ac6f91d29fd
aniledev <elinamcgill@gmail.com>
2021-02-23 12:37:21 -0500

    FEAT: create NotFoundView component

---
+++ src/pages/NotFoundView.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+export default class NotFoundView extends Component {
+  render() {
+    return <div className="NotFoundView">NOT FOUND VIEW HERE</div>;
+  }
+}
