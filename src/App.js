aeca0e71c6d9fb792f3f8f1d27ffeff0c7a8f60f
aniledev <elinamcgill@gmail.com>
2021-02-23 12:21:32 -0500

    CHORE: modify create-react-app boilerplate

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
-import './App.css';
+import "./styles/App.css";
 
 function App() {
-  return (
-    <div className="App">
-      APP COMPONENTS HERE
-    </div>
-  );
+  return <div className="App">APP COMPONENTS HERE</div>;
 }
 
 export default App;
