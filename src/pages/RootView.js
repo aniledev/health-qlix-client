70be555d29f8695894ca7bdeecfeb5c570fd050d
aniledev <elinamcgill@gmail.com>
2021-02-23 12:37:35 -0500

    FEAT: update RootView component

--- src/pages/RootView.js
+++ src/pages/RootView.js
@@ -1,10 +1,7 @@
 import React, { Component } from "react";
-import React, { Component } from "react";
 
 export default class RootView extends Component {
   render() {
-      return <div className="RootView">
-        Root View Here
-    </div>;
+    return <div className="RootView">Root View Here</div>;
   }
 }
