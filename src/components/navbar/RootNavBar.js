a4fd5f6970d510fa98e0d7d21de8d9f9d45c494b
aniledev <elinamcgill@gmail.com>
2021-02-23 13:28:31 -0500

    FEAT: create RootNavBar and RootCustomerTable components

---
+++ src/components/navbar/RootNavBar.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+export default class RootNavBar extends Component {
+  render() {
+    return <div className="RootNavBar">Goes here</div>;
+  }
+}
---
+++ src/components/table/RootCustomerTable.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+export default class RootCustomerTable extends Component {
+  render() {
+    return <div className="RootCustomerTable">Goes here</div>;
+  }
+}
